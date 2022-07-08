b97ef8a68b1c4fd1c98ae61274f32aac87cce6b3
mihai1807 <moise_mihai@ymail.com>
2022-07-08 09:45:53 +0300

    Added comments to the Main.java class to better explain the program

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        //Array lists to store Course, Student and Tutor class objects
         ArrayList<Course> allCourses = new ArrayList<>();
         ArrayList<Student> allStudents = new ArrayList<>();
         ArrayList<Tutor> allTutors = new ArrayList<>();
+
         Scanner userinput = new Scanner(System.in);
         int choice = 0;
+
+        //main program starts, end if the user enter the number 8
         while (choice!=8) {
             mainMenu();
             choice = userinput.nextInt();
             switch (choice) {
                 case (1):
+                    //prints an overview of all students, courses and tutors
                     int orderNumber = 1;
                     System.out.println("The courses are\n");
                     for (Course elem : allCourses) {
@@ -38,39 +44,49 @@ public class Main {
                     }
                     break;
                 case (2):
+                    // creates a new Student class object and adds it to the Student ArrayList
                     Student newStudent = new Student();
                     newStudent.addStudent();
                     allStudents.add(newStudent);
                     break;
                 case (3):
+                    // creates a new Tutor class object and adds it to the Tutor ArrayList
                     Tutor newTutor = new Tutor();
                     newTutor.addTutor();
                     allTutors.add(newTutor);
                     break;
                 case (4):
+                    // creates a new Course class object and adds it to the Course ArrayList
                     Course newCourse = new Course();
                     newCourse.addCourse();
                     allCourses.add(newCourse);
                     break;
                 case (5):
+                    //Removes a student based on the order all Student class objects were created and added in
                     System.out.println("Type in the number corresponding the student: ");
                     choice = userinput.nextInt();
+                    //Takes into account the fact that user will not know to use 0 as an index value
                     allStudents.remove(choice-1);
                     break;
                 case (6):
+                    //Removes a tutor based on the order all Tutor class objects were created and added in
                     System.out.println("Type in the number corresponding the tutor: ");
                     choice = userinput.nextInt();
+                    //Takes into account the fact that user will not know to use 0 as an index value
                     allTutors.remove(choice-1);
                     break;
                 case (7):
+                    //Removes a course based on the order all Course class objects were created and added in
                     System.out.println("Type in the number corresponding the course: ");
                     choice = userinput.nextInt();
+                    //Takes into account the fact that user will not know to use 0 as an index value
                     allCourses.remove(choice-1);
                     break;
             }
         }
 
     }
+    //Display the main menu
     public static void mainMenu() {
         System.out.println("--------------------------------");
         System.out.println("Welcome to your tutoring school\n" + "What would you like to do?\n");
